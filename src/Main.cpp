208fba8611e4630d54a466012394715255335e23
Kotbenek <dominik.piatkowski@protonmail.com>
2022-05-24 22:08:31 +0200

    API specification, version 1.0

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,324 +9,82 @@
 
 int main(int argc, char** argv)
 {
-    //Debug
+    std::string program_name = argv[0];
     
-    Array* a;
-    
-    std::cout << "Data -> Base64" << "\r\n";
-    
-    a = new Array(1);
-    a->data[0] = 'f';
-    std::cout << "f            " << Base64::data_to_base64(a) << "\r\n";
-    a->~Array();
-    
-    a = new Array(2);
-    a->data[0] = 'f';
-    a->data[1] = 'o';
-    std::cout << "fo           " << Base64::data_to_base64(a) << "\r\n";
-    a->~Array();
-    
-    a = new Array(3);
-    a->data[0] = 'f';
-    a->data[1] = 'o';
-    a->data[2] = 'o';
-    std::cout << "foo          " << Base64::data_to_base64(a) << "\r\n";
-    a->~Array();
-    
-    a = new Array(4);
-    a->data[0] = 'f';
-    a->data[1] = 'o';
-    a->data[2] = 'o';
-    a->data[3] = 'b';
-    std::cout << "foob         " << Base64::data_to_base64(a) << "\r\n";
-    a->~Array();
-    
-    a = new Array(5);
-    a->data[0] = 'f';
-    a->data[1] = 'o';
-    a->data[2] = 'o';
-    a->data[3] = 'b';
-    a->data[4] = 'a';
-    std::cout << "fooba        " << Base64::data_to_base64(a) << "\r\n";
-    a->~Array();
-    
-    a = new Array(6);
-    a->data[0] = 'f';
-    a->data[1] = 'o';
-    a->data[2] = 'o';
-    a->data[3] = 'b';
-    a->data[4] = 'a';
-    a->data[5] = 'r';
-    std::cout << "foobar       " << Base64::data_to_base64(a) << "\r\n";
-    a->~Array();
-    
-    std::cout << "\r\n" << "Base64 -> Data" << "\r\n";
-        
-    a = Base64::base64_to_data((char*)"Zg==");
-    std::cout << "Zg==         " << a->data << "\r\n";
-    a->~Array();
-    
-    a = Base64::base64_to_data((char*)"Zm8=");
-    std::cout << "Zm8=         " << a->data << "\r\n";
-    a->~Array();
-    
-    a = Base64::base64_to_data((char*)"Zm9v");
-    std::cout << "Zm9v         " << a->data << "\r\n";
-    a->~Array();
-    
-    a = Base64::base64_to_data((char*)"Zm9vYg==");
-    std::cout << "Zm9vYg==     " << a->data << "\r\n";
-    a->~Array();
-    
-    a = Base64::base64_to_data((char*)"Zm9vYmE=");
-    std::cout << "Zm9vYmE=     " << a->data << "\r\n";
-    a->~Array();
-    
-    a = Base64::base64_to_data((char*)"Zm9vYmFy");
-    std::cout << "Zm9vYmFy     " << a->data << "\r\n";
-    a->~Array();
-    
-    std::cout << "\r\n" << "Data -> Base16" << "\r\n";
-    
-    a = new Array(1);
-    a->data[0] = 'f';
-    std::cout << "f            " << Base16::data_to_base16(a) << "\r\n";
-    a->~Array();
-    
-    a = new Array(2);
-    a->data[0] = 'f';
-    a->data[1] = 'o';
-    std::cout << "fo           " << Base16::data_to_base16(a) << "\r\n";
-    a->~Array();
-    
-    a = new Array(3);
-    a->data[0] = 'f';
-    a->data[1] = 'o';
-    a->data[2] = 'o';
-    std::cout << "foo          " << Base16::data_to_base16(a) << "\r\n";
-    a->~Array();
-    
-    a = new Array(4);
-    a->data[0] = 'f';
-    a->data[1] = 'o';
-    a->data[2] = 'o';
-    a->data[3] = 'b';
-    std::cout << "foob         " << Base16::data_to_base16(a) << "\r\n";
-    a->~Array();
-    
-    a = new Array(5);
-    a->data[0] = 'f';
-    a->data[1] = 'o';
-    a->data[2] = 'o';
-    a->data[3] = 'b';
-    a->data[4] = 'a';
-    std::cout << "fooba        " << Base16::data_to_base16(a) << "\r\n";
-    a->~Array();
-    
-    a = new Array(6);
-    a->data[0] = 'f';
-    a->data[1] = 'o';
-    a->data[2] = 'o';
-    a->data[3] = 'b';
-    a->data[4] = 'a';
-    a->data[5] = 'r';
-    std::cout << "foobar       " << Base16::data_to_base16(a) << "\r\n";
-    a->~Array();
-    
-    std::cout << "\r\n" << "Base16 -> Data" << "\r\n";
-    
-    a = Base16::base16_to_data((char*)"66");
-    std::cout << "66           " << a->data << "\r\n";
-    a->~Array();
-    
-    a = Base16::base16_to_data((char*)"666F");
-    std::cout << "666F         " << a->data << "\r\n";
-    a->~Array();
-    
-    a = Base16::base16_to_data((char*)"666F6F");
-    std::cout << "666F6F       " << a->data << "\r\n";
-    a->~Array();
-    
-    a = Base16::base16_to_data((char*)"666F6F62");
-    std::cout << "666F6F62     " << a->data << "\r\n";
-    a->~Array();
-    
-    a = Base16::base16_to_data((char*)"666F6F6261");
-    std::cout << "666F6F6261   " << a->data << "\r\n";
-    a->~Array();
-    
-    a = Base16::base16_to_data((char*)"666F6F626172");
-    std::cout << "666F6F626172 " << a->data << "\r\n";
-    a->~Array();
-    
-    
-    AES* aes;
-    
-    std::cout << "\r\n" << "AES -> Encryption" << "\r\n";
-    
-    aes = new AES(AES::key_length::AES256);
-    
-    a = Base16::base16_to_data((char*)"603DEB1015CA71BE2B73AEF0857D77811F352C073B6108D72D9810A30914DFF4");
-    aes->set_key(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"000102030405060708090A0B0C0D0E0F");
-    aes->set_IV(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"6BC1BEE22E409F96E93D7E117393172A");
-    aes->encrypt_CBC(a);
-    std::cout << Base16::data_to_base16(a) << "\r\n";
-    std::cout << "F58C4C04D6E5F1BA779EABFB5F7BFBD6" << "\r\n";
-    delete a;
-    
-    std::cout << "\r\n" << "AES -> Decryption" << "\r\n";
-    
-    a = Base16::base16_to_data((char*)"603DEB1015CA71BE2B73AEF0857D77811F352C073B6108D72D9810A30914DFF4");
-    aes->set_key(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"000102030405060708090A0B0C0D0E0F");
-    aes->set_IV(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"F58C4C04D6E5F1BA779EABFB5F7BFBD6");
-    aes->decrypt_CBC(a);
-    std::cout << Base16::data_to_base16(a) << "\r\n";
-    std::cout << "6BC1BEE22E409F96E93D7E117393172A" << "\r\n";
-    delete a;
-    
-    std::cout << "\r\n" << "AES -> Encryption (CBC)" << "\r\n";
-    
-    a = Base16::base16_to_data((char*)"603DEB1015CA71BE2B73AEF0857D77811F352C073B6108D72D9810A30914DFF4");
-    aes->set_key(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"000102030405060708090A0B0C0D0E0F");
-    aes->set_IV(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"6BC1BEE22E409F96E93D7E117393172AAE2D8A571E03AC9C9EB76FAC45AF8E5130C81C46A35CE411E5FBC1191A0A52EFF69F2445DF4F9B17AD2B417BE66C3710");
-    aes->encrypt_CBC(a);
-    std::cout << Base16::data_to_base16(a) << "\r\n";
-    std::cout << "F58C4C04D6E5F1BA779EABFB5F7BFBD69CFC4E967EDB808D679F777BC6702C7D39F23369A9D9BACFA530E26304231461B2EB05E2C39BE9FCDA6C19078C6A9D1B" << "\r\n";
-    delete a;
-    
-    std::cout << "\r\n" << "AES -> Decryption (CBC)" << "\r\n";
-    
-    a = Base16::base16_to_data((char*)"603DEB1015CA71BE2B73AEF0857D77811F352C073B6108D72D9810A30914DFF4");
-    aes->set_key(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"000102030405060708090A0B0C0D0E0F");
-    aes->set_IV(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"F58C4C04D6E5F1BA779EABFB5F7BFBD69CFC4E967EDB808D679F777BC6702C7D39F23369A9D9BACFA530E26304231461B2EB05E2C39BE9FCDA6C19078C6A9D1B");
-    aes->decrypt_CBC(a);
-    std::cout << Base16::data_to_base16(a) << "\r\n";
-    std::cout << "6BC1BEE22E409F96E93D7E117393172AAE2D8A571E03AC9C9EB76FAC45AF8E5130C81C46A35CE411E5FBC1191A0A52EFF69F2445DF4F9B17AD2B417BE66C3710" << "\r\n";
-    delete a;
-    
-    
-    std::cout << "\r\n" << "AES -> Encryption (CBC PKCS#7)" << "\r\n";
-    
-    a = Base16::base16_to_data((char*)"603DEB1015CA71BE2B73AEF0857D77811F352C073B6108D72D9810A30914DFF4");
-    aes->set_key(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"000102030405060708090A0B0C0D0E0F");
-    aes->set_IV(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"C0FFEE");
-    Array* b = aes->encrypt_CBC_PKCS7(a);
-    std::cout << Base16::data_to_base16(b) << "\r\n";
-    std::cout << "3FE21532E00E1364B570ECA703066229" << "\r\n";
-    delete a;
-    
-    std::cout << "\r\n" << "AES -> Decryption (CBC PKCS#7)" << "\r\n";
-    
-    a = Base16::base16_to_data((char*)"603DEB1015CA71BE2B73AEF0857D77811F352C073B6108D72D9810A30914DFF4");
-    aes->set_key(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"000102030405060708090A0B0C0D0E0F");
-    aes->set_IV(a);
-    delete a;
-    
-    a = aes->decrypt_CBC_PKCS7(b);
-    std::cout << Base16::data_to_base16(a) << "\r\n";
-    std::cout << "C0FFEE" << "\r\n";
-    delete a;
-    delete b;
-    
-    std::cout << "\r\n" << "AES -> Encryption (CBC PKCS#7)" << "\r\n";
-    
-    a = Base16::base16_to_data((char*)"603DEB1015CA71BE2B73AEF0857D77811F352C073B6108D72D9810A30914DFF4");
-    aes->set_key(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"000102030405060708090A0B0C0D0E0F");
-    aes->set_IV(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"6BC1BEE22E409F96E93D7E117393172A");
-    b = aes->encrypt_CBC_PKCS7(a);
-    std::cout << Base16::data_to_base16(b) << "\r\n";
-    std::cout << "F58C4C04D6E5F1BA779EABFB5F7BFBD6485A5C81519CF378FA36D42B8547EDC0" << "\r\n";
-    delete a;
-    
-    std::cout << "\r\n" << "AES -> Decryption (CBC PKCS#7)" << "\r\n";
-    
-    a = Base16::base16_to_data((char*)"603DEB1015CA71BE2B73AEF0857D77811F352C073B6108D72D9810A30914DFF4");
-    aes->set_key(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"000102030405060708090A0B0C0D0E0F");
-    aes->set_IV(a);
-    delete a;
-    
-    a = aes->decrypt_CBC_PKCS7(b);
-    std::cout << Base16::data_to_base16(a) << "\r\n";
-    std::cout << "6BC1BEE22E409F96E93D7E117393172A" << "\r\n";
-    delete a;
-    delete b;
-    
-    std::cout << "\r\n" << "AES -> Encryption (CBC PKCS#7)" << "\r\n";
-    
-    a = Base16::base16_to_data((char*)"603DEB1015CA71BE2B73AEF0857D77811F352C073B6108D72D9810A30914DFF4");
-    aes->set_key(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"000102030405060708090A0B0C0D0E0F");
-    aes->set_IV(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"C0FFEE01020304050607080910111213141516");
-    b = aes->encrypt_CBC_PKCS7(a);
-    std::cout << Base16::data_to_base16(b) << "\r\n";
-    std::cout << "743D8CC55C5851F573E943B31A0E02CE25F581899A2E251375482C724E8850D4" << "\r\n";
-    delete a;
-    
-    std::cout << "\r\n" << "AES -> Decryption (CBC PKCS#7)" << "\r\n";
-    
-    a = Base16::base16_to_data((char*)"603DEB1015CA71BE2B73AEF0857D77811F352C073B6108D72D9810A30914DFF4");
-    aes->set_key(a);
-    delete a;
-    
-    a = Base16::base16_to_data((char*)"000102030405060708090A0B0C0D0E0F");
-    aes->set_IV(a);
-    delete a;
-    
-    a = aes->decrypt_CBC_PKCS7(b);
-    std::cout << Base16::data_to_base16(a) << "\r\n";
-    std::cout << "C0FFEE01020304050607080910111213141516" << "\r\n";
-    delete a;
-    delete b;
-    
-    delete aes;
+    //Display help
+    display_help(program_name);
     
     //Exit program
     return 0;
+}
+
+void display_help(std::string program_name)
+{
+    std::cout << "\r\n";
+    std::cout << "Usage: " << program_name << " input output action algorithm" << "\r\n";
+    std::cout << "\r\n";
+    std::cout << "Options:" << "\r\n";
+    std::cout << "    input:" << "\r\n";
+    std::cout << "        -i b16 data_in_base16" << "\r\n";
+    std::cout << "        --input base16 data_in_base16" << "\r\n";
+    std::cout << "            Input data provided in base16 format" << "\r\n";
+    std::cout << "        -i b64 data_in_base64" << "\r\n";
+    std::cout << "        --input base64 data_in_base64" << "\r\n";
+    std::cout << "            Input data provided in base64 format" << "\r\n";
+    std::cout << "        -i f filename" << "\r\n";
+    std::cout << "        --input file filename" << "\r\n";
+    std::cout << "            Input data provided in specified file" << "\r\n";
+    std::cout << "    output:" << "\r\n";
+    std::cout << "        -o b16" << "\r\n";
+    std::cout << "        --output base16" << "\r\n";
+    std::cout << "            Print output data in base16 format" << "\r\n";
+    std::cout << "        -o b64" << "\r\n";
+    std::cout << "        --output base64" << "\r\n";
+    std::cout << "            Print output data in base64 format" << "\r\n";
+    std::cout << "        -o f filename" << "\r\n";
+    std::cout << "        --output file filename" << "\r\n";
+    std::cout << "            Write output data to specified file" << "\r\n";
+    std::cout << "    action:" << "\r\n";
+    std::cout << "        -e" << "\r\n";
+    std::cout << "        --encrypt" << "\r\n";
+    std::cout << "            Encrypt" << "\r\n";
+    std::cout << "        -d" << "\r\n";
+    std::cout << "        --decrypt" << "\r\n";
+    std::cout << "            Decrypt" << "\r\n";
+    std::cout << "    algorithm:" << "\r\n";
+    std::cout << "        -a algorithm_with_parameters" << "\r\n";
+    std::cout << "        --algorithm algorithm_with_parameters" << "\r\n";
+    std::cout << "Supported algorithms:" << "\r\n";
+    std::cout << "    AES: aes key_size block_cipher_mode key iv" << "\r\n";
+    std::cout << "        key_size:" << "\r\n";
+    std::cout << "            -s size" << "\r\n";
+    std::cout << "            --key_size size" << "\r\n";
+    std::cout << "                size:" << "\r\n";
+    std::cout << "                    128" << "\r\n";
+    std::cout << "                        128-bit key" << "\r\n";
+    std::cout << "                    192" << "\r\n";
+    std::cout << "                        192-bit key" << "\r\n";
+    std::cout << "                    256" << "\r\n";
+    std::cout << "                        256-bit key" << "\r\n";
+    std::cout << "        block_cipher_mode:" << "\r\n";
+    std::cout << "            -m mode" << "\r\n";
+    std::cout << "            --block_cipher_mode mode" << "\r\n";
+    std::cout << "                mode:" << "\r\n";
+    std::cout << "                    cbc" << "\r\n";
+    std::cout << "                        Cipher block chaining" << "\r\n";
+    std::cout << "        key:" << "\r\n";
+    std::cout << "            -k b16 key_in_base16" << "\r\n";
+    std::cout << "            --key base16 key_in_base16" << "\r\n";
+    std::cout << "                Key provided in base16 format" << "\r\n";
+    std::cout << "            -k b64 key_in_base64" << "\r\n";
+    std::cout << "            --key base64 key_in_base64" << "\r\n";
+    std::cout << "                Key provided in base64 format" << "\r\n";
+    std::cout << "        iv:" << "\r\n";
+    std::cout << "            -v b16 iv_in_base16" << "\r\n";
+    std::cout << "            --iv base16 iv_in_base16" << "\r\n";
+    std::cout << "                IV provided in base16 format" << "\r\n";
+    std::cout << "            -v b64 iv_in_base64" << "\r\n";
+    std::cout << "            --iv base64 iv_in_base64" << "\r\n";
+    std::cout << "                IV provided in base64 format" << "\r\n";
+    std::cout << "\r\n";
 }
--- src/Main.h
+++ src/Main.h
@@ -1,3 +1,6 @@
 #pragma once
 
+#include <string>
+
 int main(int argc, char** argv);
+void display_help(std::string program_name);
