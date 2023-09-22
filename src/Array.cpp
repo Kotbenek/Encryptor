d0d01bff7bea0af8344bdaf84f0bf0a787c70a9a
Kotbenek <dominik.piatkowski@protonmail.com>
2023-09-22 14:57:14 +0200

    Implemented Array::resize
    This patch introduces resize functionality to Array.
    
    Switched to malloc in Array constructors, so the resize functionality
    can be efficiently done with realloc.

--- src/Array.cpp
+++ src/Array.cpp
@@ -7,21 +7,27 @@ uint64_t Array::size()
     return _size;
 }
 
+void Array::resize(uint64_t new_size)
+{
+    data = (uint8_t*)realloc(data, new_size);
+    _size = new_size;
+}
+
 Array::Array(uint64_t size)
 {
-    data = new uint8_t[size];
+    data = (uint8_t*)malloc(size);
     _size = size;
 }
 
 Array::Array(Array& a)
 {
     _size = a.size();
-    data = new uint8_t[_size];
+    data = (uint8_t*)malloc(_size);
 
     std::copy(&a.data[0], &a.data[a.size()], data);
 }
 
 Array::~Array()
 {
-    delete[] data;
+    free(data);
 }
--- src/Array.h
+++ src/Array.h
@@ -8,6 +8,7 @@ public:
     uint8_t* data;
 
     uint64_t size();
+    void resize(uint64_t new_size);
 
     Array(uint64_t size);
     Array(Array& a);
