c6d306eca354083107813c809f2392a5d861fa75
Kotbenek <dominik.piatkowski@protonmail.com>
2023-09-22 15:04:27 +0200

    Implemented standalone PKCS7 padding
    This patch introduces standalone PKCS7 padding implementation which will
    help with separating functions like encrypt_file_CBC_PKCS7.

---
+++ src/Padding.cpp
@@ -0,0 +1,32 @@
+#include "Padding.h"
+
+#include "Array.h"
+
+#include <cstdint>
+
+namespace Padding
+{
+    void PKCS7::append(Array* data, uint8_t alignment)
+    {
+        uint64_t old_size = data->size();
+        uint8_t padding_value = alignment - (old_size % alignment);
+        data->resize(old_size + padding_value);
+        for (uint64_t i = old_size; i < data->size(); i++)
+            data->data[i] = padding_value;
+    }
+
+    bool PKCS7::check(Array* data)
+    {
+        uint8_t padding_value = data->data[data->size() - 1];
+        for (uint64_t i = data->size() - padding_value; i < data->size(); i++)
+            if (data->data[i] != padding_value)
+                return false;
+        return true;
+    }
+
+    void PKCS7::remove(Array* data)
+    {
+        uint8_t padding_value = data->data[data->size() - 1];
+        data->resize(data->size() - padding_value);
+    }
+} //namespace Padding
---
+++ src/Padding.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Array.h"
+
+#include <cstdint>
+
+namespace Padding
+{
+    class PKCS7
+    {
+    public:
+        static void append(Array* data, uint8_t alignment);
+        static bool check(Array* data);
+        static void remove(Array* data);
+    };
+} //namespace Padding
