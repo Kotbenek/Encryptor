18680ccbefbcd1bc1c9feabb2b8241dcf4e0d51a
Kotbenek <dominik.piatkowski@protonmail.com>
2023-11-05 15:11:34 +0100

    Implemented ANSI X9.23 padding

--- src/Padding.cpp
+++ src/Padding.cpp
@@ -31,4 +31,31 @@ namespace Padding
         uint8_t padding_value = data->data[data->size() - 1];
         data->resize(data->size() - padding_value);
     }
+
+    void ANSI_X923::append(Array* data, uint8_t alignment)
+    {
+        uint64_t old_size = data->size();
+        uint8_t padding_length = alignment - (old_size % alignment);
+        data->resize(old_size + padding_length);
+        for (uint64_t i = old_size; i < data->size() - 1; i++)
+            data->data[i] = 0;
+        data->data[data->size() - 1] = padding_length;
+    }
+
+    bool ANSI_X923::check(Array* data)
+    {
+        uint8_t padding_length = data->data[data->size() - 1];
+        if (padding_length > data->size())
+            return false;
+        for (uint64_t i = data->size() - padding_length; i < data->size() - 1; i++)
+            if (data->data[i] != 0)
+                return false;
+        return true;
+    }
+
+    void ANSI_X923::remove(Array* data)
+    {
+        uint8_t padding_length = data->data[data->size() - 1];
+        data->resize(data->size() - padding_length);
+    }
 } //namespace Padding
--- src/Padding.h
+++ src/Padding.h
@@ -13,4 +13,12 @@ namespace Padding
         static bool check(Array* data);
         static void remove(Array* data);
     };
+
+    class ANSI_X923
+    {
+    public:
+        static void append(Array* data, uint8_t alignment);
+        static bool check(Array* data);
+        static void remove(Array* data);
+    };
 } //namespace Padding
